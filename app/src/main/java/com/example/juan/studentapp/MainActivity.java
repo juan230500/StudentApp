576c49ce5c5c81051e4a97b925b8a82bfb533522
juan <juanpo.alvarado@gmail.com>
2018-11-17 20:22:10 -0600

    Movimiento de imagen por boton
    este prototipo se puede adaptar a re colocar un icono de chófer cada vez que el estudiante presione el botón, estas coordenadas se deben obtener desde el servidor

--- app/src/main/java/com/example/juan/studentapp/MainActivity.java
+++ app/src/main/java/com/example/juan/studentapp/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.juan.studentapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView img;
+    private int y=0;
+    private int x=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img=findViewById(R.id.image);
+    }
+
+    public void go(View view){
+        y+=100;
+        x+=50;
+        img.setTranslationY(y);
+        img.setTranslationX(x);
     }
 }
