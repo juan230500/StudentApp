524ef6b786ea2e0a13004c3eb74ad729762c93f6
juan <juanpo.alvarado@gmail.com>
2018-11-17 21:59:09 -0600

    Hilo de movimiento

--- app/src/main/java/com/example/juan/studentapp/MainActivity.java
+++ app/src/main/java/com/example/juan/studentapp/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.juan.studentapp;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
     private ImageView img;
     private int y=0;
     private int x=0;
+    private Handler handler=new Handler();
+    private Timer timer=new Timer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +23,20 @@ public class MainActivity extends AppCompatActivity {
         img=findViewById(R.id.image);
     }
 
-    public void go(View view){
-        y+=100;
-        x+=50;
-        img.setTranslationY(y);
-        img.setTranslationX(x);
+    public void go(View view) {
+        //https://www.youtube.com/watch?v=UxbJKNjQWD8
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        x+=1;
+                        img.setX(x);
+                    }
+                });
+            }
+        },0,5);
     }
+
 }
