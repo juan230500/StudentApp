4941f13f04ca9e95f5bfeda3c6eba4ea0960434d
juan <juanpo.alvarado@gmail.com>
2018-11-21 15:02:06 -0600

    Dibujar Lineas

--- app/src/main/java/com/example/juan/studentapp/MainActivity.java
+++ app/src/main/java/com/example/juan/studentapp/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.juan.studentapp;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,48 +14,45 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-    private ImageView img;
-    private float y;
-    private float x;
-    private float xf;
-    private float yf;
-    private float m;
-    private float b;
-    private Handler handler=new Handler();
-    private Timer timer=new Timer();
+    private int y;
+    private int x;
+    Canvas canvas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        img=findViewById(R.id.image);
+
+        ImageView drawingImageView = (ImageView) this.findViewById(R.id.image);
+        Bitmap bitmap = Bitmap.createBitmap((int) getWindowManager()
+                .getDefaultDisplay().getWidth(), (int) getWindowManager()
+                .getDefaultDisplay().getHeight(), Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+        drawingImageView.setImageBitmap(bitmap);
+
+        // Line
+
+
     }
 
     public void go(View view) {
-        y=100;
-        x=100;
-        xf=200;
-        yf=500;
-        m=(yf-y)/(xf-x);
-        b=y-m*x;
-        //https://www.youtube.com/watch?v=UxbJKNjQWD8
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
+        Paint paint = new Paint();
+        paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(5);
+        int[] ArrY={100,200};
+        int[] ArrX={50,150};
+        x=500;
+        y=500;
+        for (int i=0;i<ArrY.length;i++){
+            canvas.drawLine(x, y,ArrX[i] , ArrY[i], paint);
+            x=ArrX[i];
+            y=ArrY[i];
+        }
 
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (x<xf){
-                            x+=2;
-                            y=x*m+b;
-                            img.setX(x);
-                            img.setY(y);
-                        }
-                    }
-                });
-            }
-        },0,50);
+    }
+
+    public void DrawLines(){
+
     }
 
 }
