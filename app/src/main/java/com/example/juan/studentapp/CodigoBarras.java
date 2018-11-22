e9a2e94d67bd35f702d74eea73da939f2351b05e
José Solano <joseandressolanomora@gmail.com>
2018-11-21 20:35:24 -0600

    Almacenamiento en ficheros y validaciones
    Se hace uso de ficheros para almacenar el carné de registro, además se crean validaciones.

--- app/src/main/java/com/example/juan/studentapp/CodigoBarras.java
+++ app/src/main/java/com/example/juan/studentapp/CodigoBarras.java
@@ -1,31 +1,29 @@
 package com.example.juan.studentapp;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.Toast;
-
 import com.google.zxing.Result;
-
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class CodigoBarras extends AppCompatActivity implements ZXingScannerView.ResultHandler {
-
-    private EditText codigoBarras;
+    private String codigoBarras;
     private ZXingScannerView scannerView;
     private int codigoPermiso = 1;
 
 
-    public void Escanear(View view){
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+    public void Escanear(View view) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
             this.scannerView = new ZXingScannerView(this);
             setContentView(this.scannerView);
             scannerView.setResultHandler(this);
@@ -36,8 +34,8 @@ public class CodigoBarras extends AppCompatActivity implements ZXingScannerView.
 
     }
 
-    public void pedirPermiso(){
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)){
+    public void pedirPermiso() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
 
             new AlertDialog.Builder(this)
                     .setTitle("Permisos de cámara desactivados")
@@ -45,7 +43,7 @@ public class CodigoBarras extends AppCompatActivity implements ZXingScannerView.
                     .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            ActivityCompat.requestPermissions(CodigoBarras.this, new String[] {Manifest.permission.CAMERA}, codigoPermiso);
+                            ActivityCompat.requestPermissions(CodigoBarras.this, new String[]{Manifest.permission.CAMERA}, codigoPermiso);
                         }
                     })
                     .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
@@ -55,45 +53,71 @@ public class CodigoBarras extends AppCompatActivity implements ZXingScannerView.
                         }
                     })
                     .create().show();
-
-
         } else {
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, codigoPermiso);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, codigoPermiso);
         }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == codigoPermiso){
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                Toast.makeText(this, "Permiso aprobado", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_codigo_barras);
     }
 
-   // @Override
-   // protected void onPause() {
-   //     super.onPause();
-    //    scannerView.stopCamera();
-   // }
-
-    @Override
     public void handleResult(Result result) {
         scannerView.stopCamera();
         setContentView(R.layout.activity_codigo_barras);
+        if (result.getText().length() != 10) {
+            new AlertDialog.Builder(this)
+                    .setTitle("El codigo escaneado no pertenece a un carné TEC")
+                    .setMessage("Por favor intentelo de nuevo")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).create().show();
+        } else {
+            this.codigoBarras = result.getText();
+            new AlertDialog.Builder(this)
+                    .setTitle("Su carné es")
+                    .setMessage(this.codigoBarras)
+                    .setPositiveButton("Correcto", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            guardar();
+                            dialog.dismiss();
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            new AlertDialog.Builder(CodigoBarras.this)
+                                    .setTitle("Vuelva a intentarlo")
+                                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            dialog.dismiss();
+                                        }
+                                    }).create().show();
+                        }
+                    })
+                    .create().show();
+        }
+    }
+
+    public void guardar(){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Resultado de escaneo");
-        builder.setMessage(result.getText());
+        builder.setTitle("Se ha registrado el carné");
+        builder.setMessage(codigoBarras);
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
-        //scannerView.resumeCameraPreview(this);
+        Toast.makeText(this, "Amigo agregado!", Toast.LENGTH_LONG);
+        try {
+            OutputStreamWriter archivo_wr = new OutputStreamWriter(openFileOutput("micarne.txt", Activity.MODE_PRIVATE));
+            archivo_wr.write(codigoBarras);
+            archivo_wr.flush();
+            archivo_wr.close();
+        } catch (IOException e){}
     }
 }
